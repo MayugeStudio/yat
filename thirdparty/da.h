17708fa4ea1244f8f0292d896f81b798e379158c
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-10-04 22:10:56 +0900

    Fix DA_APPEND_MANY bug

--- thirdparty/da.h
+++ thirdparty/da.h
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define DA_INIT_CAPACITY 8
 #define DA_MALLOC malloc
@@ -11,40 +12,42 @@
       if ((da)->capacity == 0) {                                \
         (da)->capacity = DA_INIT_CAPACITY;                      \
       }                                                         \
-      while ((target_capacity) < (da)->capacity) {              \
+      while ((target_capacity) > (da)->capacity) {              \
         (da)->capacity *= 2;                                    \
       }                                                         \
-      (da)->items = DA_REALLOC((da)->items, (da)->capacity);    \
+      (da)->items = DA_REALLOC((da)->items, (da)->capacity * sizeof(*(da)->items));    \
+      if (!(da)->items) DA_ASSERT(false && "buy more RAM lol");                      \
     } while (0)
 
-#define DA_APPEND(da, item)                                             \
-  do {                                                                  \
-    if ((da)->capacity == 0) {                                          \
-      (da)->capacity = DA_INIT_CAPACITY;                                \
-      (da)->count = 0;                                                  \
-      (da)->items = DA_MALLOC(DA_INIT_CAPACITY * sizeof(*(da)->items)); \
-      if (!(da)->items) DA_ASSERT(false && "buy more RAM lol");         \
-    } else if ((da)->capacity == (da)->count) {                         \
-      (da)->capacity = (da)->capacity * 2;                              \
-      (da)->items = DA_REALLOC((da)->items, (da)->capacity);            \
-    }                                                                   \
-    (da)->items[(da)->count] = item;                                    \
-    (da)->count++;                                                      \
+#define DA_APPEND(da, item)                                                          \
+  do {                                                                               \
+    if ((da)->capacity == 0) {                                                       \
+      (da)->capacity = DA_INIT_CAPACITY;                                             \
+      (da)->count = 0;                                                               \
+      (da)->items = DA_MALLOC(DA_INIT_CAPACITY * sizeof(*(da)->items));              \
+      if (!(da)->items) DA_ASSERT(false && "buy more RAM lol");                      \
+    } else if ((da)->capacity == (da)->count) {                                      \
+      (da)->capacity = (da)->capacity * 2;                                           \
+      (da)->items = DA_REALLOC((da)->items, (da)->capacity * sizeof(*(da)->items));  \
+      if (!(da)->items) DA_ASSERT(false && "buy more RAM lol");                      \
+    }                                                                                \
+    (da)->items[(da)->count] = item;                                                 \
+    (da)->count++;                                                                   \
   } while (0)
 
 #define DA_APPEND_MANY(da, elems, items_count)                                          \
   do {                                                                                  \
     if ((da)->capacity == 0) {                                                          \
       (da)->capacity = DA_INIT_CAPACITY;                                                \
+      (da)->count = 0;                                                                  \
+      (da)->items = DA_MALLOC(DA_INIT_CAPACITY * sizeof(*(da)->items));                 \
+      if (!(da)->items) DA_ASSERT(false && "buy more RAM lol");                      \
     }                                                                                   \
-    if ((items_count) > (da)->capacity) {                                               \
-      while ((items_count) > (da)->capacity) {                                          \
-        (da)->capacity *= 2;                                                            \
-      }                                                                                 \
-      (da)->items = DA_REALLOC((da)->items, (da)->capacity);                            \
-      memcpy((da)->items + (da)->count, (elems), (items_count)*sizeof(*(da)->items));   \
-      (da)->count += items_count;                                                       \
+    if ((da)->count + (items_count) > (da)->capacity) {                                               \
+      DA_RESERVE(da, (da)->count + (items_count));                                                      \
     }                                                                                   \
+    memcpy((da)->items + (da)->count, (elems), (items_count)*sizeof(*(da)->items));     \
+    (da)->count += items_count;                                                         \
   } while (0)
 
 #ifdef DA_IMPLEMENTATION
